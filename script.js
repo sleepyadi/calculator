21bcc732f5c02cf6d8f6690ca3994afe9341e244
adi <xdragon.0601@outlook.com>
2022-06-13 17:33:39 +0530

    Add methods to handle and display arithmetic operations input

--- script.js
+++ script.js
@@ -2,13 +2,34 @@ const numbers = document.querySelectorAll('.number');
 const calcContainer = document.querySelector('.calculator');
 const displayEquation = document.querySelector('.equation');
 const displayCurrent = document.querySelector('.current-number');
-
+const operatorBtns = document.querySelectorAll('.operator');
 
 let currentNum = '';
 let equationArr = [];
 
 function handleNumbers(e) {
     currentNum += e.target.textContent;
+}
+
+function handleOperators(e) {
+    
+    if (currentNum === '') {
+        currentNum = (e.target.textContent === '-' || e.target.textContent === "+") ? e.target.textContent : '';
+    } else if (!isNaN(currentNum)) {
+        equationArr.push(Number(currentNum));
+        equationArr.push(e.target.textContent);
+        currentNum = '';
+    } else {
+
+        if (e.target.textContent === '-') {
+        
+            if (currentNum === '-') {
+                currentNum = '+';
+            } else if (currentNum === '+') {
+                currentNum = '-';
+            }
+        }
+    }
 }
 
 function updateDisplay() {
@@ -32,6 +53,12 @@ numbers.forEach((number) => {
 
     number.addEventListener('click', handleNumbers)
 });
+
+operatorBtns.forEach((operatorBtn) => {
+
+    operatorBtn.addEventListener('click', handleOperators)
+});
+
 
 calcContainer.addEventListener('click', (e) => {
     if (e.target.nodeName === 'BUTTON') {
